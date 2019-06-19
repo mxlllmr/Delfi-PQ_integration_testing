566fdf264c6ae7c63e3e4a365fc43d5fd7e9a7c1
mxlllmr <51790860+mxlllmr@users.noreply.github.com>
2019-06-19 16:31:40 +0200

    Add files via upload

--- client_ADB_noUI.py
+++ client_ADB_noUI.py
@@ -7,7 +7,7 @@ Authors: Maxi Gallbrecht, Tiago Costa
 This python script has the purpose to test the communication with the ADB
 subsystem of the Delfi-PQ.
 
-Here, the one of the four powerbusses is tested and verified through communication with
+Here, Bus1Sw of the ADB is tested and verified through communication with
 the ADB itself and an Arduino, that reads the voltage levels
 
 '''
@@ -49,7 +49,7 @@ FUNCTION process_frame
 
 '''
 def process_frame(packet):
-    print "Command received in", packet['Source'], "\n"
+    print "\nCommand received in", packet['Source'], "\n"
 
 
 '''
@@ -78,69 +78,45 @@ FUNCTION send_packets
             calls a function to turn ON or OFF the powerbus.
 
 '''
-def send_packets(busNumber):
+def send_packets():
     global user
     global last_received
     global working
-    while working:
-        user_previous=user
-        user_input(user_previous) # gets and verifies user input
-        if not working:
-            break      # If the program finished while the thread was in 'input'
 
-        if user==0:
-            sys.stdout.write('\nCommand received.\nSending to board->') # print with no '\n'
-            pq_class.busOFF("ADB",busNumber)
-            packets = pq_class.get_packets()
-            time.sleep(1)
-            if packets:
-                for packet in packets:
-                    process_frame(packet)
+    print('\nBus1Sw will now be turned ON and then turned OFF.')
+    user=1
+    sys.stdout.write('\nTurning Bus1Sw ON.\nSending command to board->')
 
-            check(0) # Immediatly check the arduino
-        elif user==1:
-            sys.stdout.write('\nCommand received.\nSending to board->')
-            #insert bus command
-            pq_class.busON("ADB", busNumber)
-            packets = pq_class.get_packets()
-            time.sleep(1)
-            if packets:
-                for packet in packets:
-                    process_frame(packet)
+    pq_class.busON("ADB", 'Bus1Sw')
+    packets = pq_class.get_packets()
+    time.sleep(1)
+    if packets:
+        for packet in packets:
+            process_frame(packet)
+    time.sleep(1)
+    check() # check the arduino
 
-            check(0) # Immediatly check the arduino
+    time.sleep(3)
 
-'''
-FUNCTION user_input
+    user=0
+    sys.stdout.write('\nTurning Bus1Sw OFF.\nSending command to board->') # print with no '\n'
 
-    Input:user_previous
-    Output:None
-    Purpose:To interpret the user input in the terminal. If it's not 0 or 1, it
-            uses the previous valid command. After the first input it changes
-            the 'first_input' flag
-'''
-def user_input(user_previous):
-    global user
-    global working
-    global first_input
+    pq_class.busOFF("ADB",'Bus1Sw')
+    packets = pq_class.get_packets()
+    time.sleep(1)
+    if packets:
+        for packet in packets:
+            process_frame(packet)
+    time.sleep(1)
+    check() # check the arduino
 
-    while 1:
-        try:
-            print('\nChoose ON (press 1) or OFF (press 0).')
-            user=input()
-        except:
-            if working==False:
-                break
-            else:
-                print("The command is incorrect. Try again.")
-                user=user_previous
-        else:
-            if user==0 or user==1:
-                first_input=True
-                break
-            else:
-                print("The command is incorrect. Try again.")
-                user=user_previous
+    time.sleep(3)
+    print('\nProgramme done.')
+    working=False
+    time.sleep(2)
+    print('Exit.')
+    sys.exit()
+
 
 
 '''
@@ -181,33 +157,24 @@ FUNCTION check
             it breaks the 'working' loop.
 '''
 
-def check(wait_time):
+def check():
     global user
     global last_received
     global working
     global first_input
 
-    while working:  # Wait time has to be !=0 for recheck and =0 for immediate check
-        if first_input==True:
-            if user==1 and last_received=='1':
-                print('Arduino: The BUS '+busNumber+' is ON.')
-            elif user==0 and last_received=='0':
-                print('Arduino: The BUS '+busNumber+' is OFF.')
-            elif user==0 and last_received=='1':
-                print('\nERROR: The BUS '+busNumber+' is ON, despite the input. Logfile generated')
-                log()
-                print("Input set to 1\n")
-                user=1
-            elif user==1 and last_received=='0':
-                print('\nERROR: The BUS '+busNumber+' is OFF, despite the input. Logfile generated')
-                log()
-                print("Input set to 0\n")
-                user=0
+    time.sleep(1)
 
-        if wait_time==0:
-            break
-
-        time.sleep(wait_time)
+    if user==1 and last_received=='1':
+        print('Arduino: The Bus1Sw is ON.')
+    elif user==0 and last_received=='0':
+        print('Arduino: The Bus1Sw is OFF.')
+    elif user==0 and last_received=='1':
+        print('\nERROR: The Bus1Sw is ON, despite the input. Logfile generated')
+        log()
+    elif user==1 and last_received=='0':
+        print('\nERROR: The Bus1Sw is OFF, despite the input. Logfile generated')
+        log()
 
 '''
 FUNCTION log
@@ -229,7 +196,7 @@ def log():
     logging.basicConfig(filename='log_BUS.log', pathname='./log_BUS.log',
     format='%(asctime)s - %(levelname)s - %(message)s\n',
                         datefmt='%d-%b-%y %H:%M:%S')
-    logging.error('Input was '+str(user)+' and BUS '+busNumber+' was '+bus+
+    logging.error('Input was '+str(user)+' and BUS Bus1Sw was '+bus+
     '\nArduino detected an unwanted output. Check connections')
 
 '''
@@ -242,9 +209,9 @@ FUNCTION introduction
 def introduction():
     print("\n\n#################################################################")
     print("Welcome to the Power Bus verification software!")
-    print("This application prints out every 10 seconds the Arduino feedback")
-    print("Choose one of the four busses with 1 for Bus1Sw, 2 for Bus2Sw, 3 for Bus3Sw, or 4 for Bus4Sw")
-    print("The commands are: 1 to turn the BUS ON, 0 to turn it OFF.")
+    print("Ten ping commands are sent to the ADB.")
+    print("After that, Bus1Sw will be turned ON and turned OFF.")
+    print("All occuring ERRORS will be logged in log_BUS_noUI .")
     print("To exit the application press \'CTRL+C\'")
     print("#################################################################\n")
 
@@ -289,38 +256,13 @@ def choose_arduino_port():
                     connected= True
         except KeyboardInterrupt: # if ctrl+c is pressed
             print('\nExit.')
+            working=False
             sys.exit() #quit
         except:
             print("Wrong port. Try again.")
 
     return arduino
 
-
-'''
-FUNCTION choose_bus_number
-
-    Input:None
-    Output:busNumber
-    Purpose:It asks the user to choose one of the four power busses of the ADB
-            in oder to turn it ON or OFF
-'''
-
-def choose_bus_number():
-    busNumber = 0
-
-    user_bus=input('Insert the BUS number (1,2,3,4):')
-    if user_bus == 1:
-        busNumber = 'Bus1Sw'
-    elif user_bus == 2:
-        busNumber = 'Bus2Sw'
-    elif user_bus == 3:
-        busNumber = 'Bus3Sw'
-    elif user_bus == 4:
-        busNumber = 'Bus4Sw'
-    else:
-        print("The command is incorrect. Try again.")
-    print('' +busNumber+ ' was selected.')
-    return busNumber
 
 
 '''
@@ -336,7 +278,7 @@ def initialization():
     connected=False
     counter=1
     toolbar_width=10
-    sys.stdout.write('Verifying connection with the board... ')
+    sys.stdout.write('Verifying connection with the board... sending 10 pings')
     sys.stdout.flush()
 
     sys.stdout.write("[%s]" % (" " * toolbar_width))
@@ -351,17 +293,19 @@ def initialization():
             sys.stdout.flush()
             counter += 1
         else:
+            logging.basicConfig(filename='log_BUS_noUI.log', pathname='./log_BUSnoUI.log',
+                format='%(asctime)s - %(levelname)s - %(message)s\n',
+                datefmt='%d-%b-%y %H:%M:%S')
+            logging.error('Ping '+str(counter)+' was unsuccessfully sent.'
+                '\nCheck connections')
+            time.sleep(2)
             counter = 1 #If it failed one time, restarts to count
-
         if counter==11:
             print("\nConnected to the board!")
-            print("\nThe application is ready for the BUS input")
-            print("\nFirst, input 1, 2, 3 or 4 to select the ADB bus and press \'ENTER\'")
-            print("\nThen, input 1 or 0 to turn the selected bus ON or OFF and press \'ENTER\')")
             connected=True
 
         try:
-            time.sleep(0.3)
+            time.sleep(1)
         except KeyboardInterrupt:  # Exits if the user presses CTRL+C while pinging
             working=False
             print("\nExit.")
@@ -375,7 +319,7 @@ BUFFER_SIZE = 1024
 working = True      # Flag that is used to end threads when the program is exiting
 first_input=False   # Flag to only start to check after the first user input
 last_received = ''  # Arduino serial
-user=0              # User Input
+user=0              # Input
 wait_time=10        # Waiting time to recheck with Arduino
 
 fname = sys.argv[1]
@@ -391,18 +335,10 @@ t.start()
 
 initialization() # Verification of connection with the board
 
-busNumber=choose_bus_number() # Choosing one bus number 1 to 4
-
-t2=threading.Thread(target=send_packets, args=(busNumber,)) # Sending commands
+t2=threading.Thread(target=send_packets) # Sending commands
 t2.start()
 
 t3=threading.Thread(target=receiving, args=(arduino,True,)) # Arduino serial monitor
 t3.start()                                             # reading
 
-t4=threading.Thread(target=check, args=(wait_time,))  # Arduino feedback
-t4.start()
-
 signal.signal(signal.SIGINT, signal_handler)  # Controls the exit of the program
-
-while 1:
-   pass  # Infinit loop
